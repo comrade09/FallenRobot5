b470f7ff9c4faf92c96d852224a165bee8a09aba
Tyhuionk <144687378+Tyhuionk@users.noreply.github.com>
2023-09-12 14:04:43 +0530

    Create ai_mod.py
    Signed-off-by: Tyhuionk <144687378+Tyhuionk@users.noreply.github.com>

---
+++ FallenRobot/modules/ai_mod.py
@@ -0,0 +1,10 @@
+__help__ = """
+ ❍ /bard How to make coffee 
+    Google AI answer
+ ❍ /askgpt how to make coffee
+   Get AI answers by chat gpt 
+ ❍ /asksaf How to make coffee 
+   Ask safone
+"""
+
+__mod_name__ = "AI"
