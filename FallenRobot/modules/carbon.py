21a0000ff85543cee453c7e3f1b81f5f18ec8969
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-11-10 05:26:40 +0530

    Update carbon.py
    Signed-off-by: Comrade X  <106185020+comrade09@users.noreply.github.com>

--- FallenRobot/modules/carbon.py
+++ FallenRobot/modules/carbon.py
@@ -18,7 +18,7 @@ def base64_to_image(base64_data):
         print(f"Error decoding base64 data: {e}")
         return None
 
-@bot.on_message(filters.command("carbon"), group=1)
+@pbot.on_message(filters.command("carbon"), group=1)
 async def carbon_func(_, message):
     if message.reply_to_message:
         if message.reply_to_message.text:
