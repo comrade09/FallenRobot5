f4db30e3dacc57bfb4886227c7b9b24378dc6acd
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-11-10 05:17:25 +0530

    Update carbon.py
    Signed-off-by: Comrade X  <106185020+comrade09@users.noreply.github.com>

--- FallenRobot/modules/carbon.py
+++ FallenRobot/modules/carbon.py
@@ -1,12 +1,24 @@
 from pyrogram import filters
-
+import asyncio
 from FallenRobot import pbot
 from FallenRobot.utils.errors import capture_err
 from FallenRobot.utils.functions import make_carbon
+from io import BytesIO
+import base64
+import requests
+from PIL import Image
 
+def base64_to_image(base64_data):
+    try:
+        base64_data += '=' * (4 - len(base64_data) % 4)
+        image_data = base64.b64decode(base64_data)
+        image = Image.open(BytesIO(image_data))
+        return image
+    except Exception as e:
+        print(f"Error decoding base64 data: {e}")
+        return None
 
-@pbot.on_message(filters.command("carbon"))
-@capture_err
+@bot.on_message(filters.command("carbon"), group=1)
 async def carbon_func(_, message):
     if message.reply_to_message:
         if message.reply_to_message.text:
@@ -19,15 +31,29 @@ async def carbon_func(_, message):
         except IndexError:
             return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ.")
     m = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴀʀʙᴏɴ...")
-    carbon = await make_carbon(txt)
-    await m.edit_text("ᴜᴩʟᴏᴀᴅɪɴɢ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴄᴀʀʙᴏɴ...")
-    await pbot.send_photo(
-        message.chat.id,
-        photo=carbon,
-        caption=f"» ʀᴇᴏ̨ᴜᴇsᴛᴇᴅ ʙʏ : {message.from_user.mention}",
-    )
-    await m.delete()
-    carbon.close()
+    response = requests.get(f"https://api.safone.dev/carbon?code=" + txt )
+    if response.status_code == 200:  
+        data = response.json()
+        
+        # Check if 'image' key exists in the response
+        imx = data['image']
+
+
+    # Replace 'YOUR_BASE64_IMAGE_DATA' with your actual base64-encoded image data
+    base64_image_data = imx
+
+    image = base64_to_image(base64_image_data)
+    if image:
+        # Convert image to BytesIO object
+        image_buffer = BytesIO()
+        image.save(image_buffer, format="PNG")
+        image_buffer.seek(0)
+
+        # Send image to the user who requested
+        await message.reply_photo(image_buffer, caption=f"Here is your image! {message.from_user.mention} ")
+        await m.delete()
+    else:
+        await message.reply_text("Failed to decode base64 image data.")
 
 
 __mod_name__ = "Cᴀʀʙᴏɴ"
