526b0669c73ec2dafb74df5785fc880b1b1dca8f
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-11-10 14:52:27 +0530

    Update carbon.py
    Signed-off-by: Comrade X  <106185020+comrade09@users.noreply.github.com>

--- FallenRobot/modules/carbon.py
+++ FallenRobot/modules/carbon.py
@@ -18,20 +18,57 @@ def base64_to_image(base64_data):
         print(f"Error decoding base64 data: {e}")
         return None
 
-@pbot.on_message(filters.command("carbon"), group=1)
+@pbot.on_message(filters.command("carbon"), group=35)
 async def carbon_func(_, message):
     if message.reply_to_message:
         if message.reply_to_message.text:
             txt = message.reply_to_message.text
         else:
-            return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ.")
+            return await message.reply_text("Please Reply to a message")
+    else:
+        try:
+            txt = message.text.split(None, 1)[1]
+        except IndexError:
+            return await message.reply_text("Please Reply to a message")
+    m = await message.reply_text("Generating Carbon....")
+    response = requests.get(f"https://api.safone.dev/carbon?code=" + txt )
+    if response.status_code == 200:  
+        data = response.json()
+        
+        # Check if 'image' key exists in the response
+        imx = data['image']
+
+
+    # Replace 'YOUR_BASE64_IMAGE_DATA' with your actual base64-encoded image data
+    base64_image_data = imx
+
+    image = base64_to_image(base64_image_data)
+    if image:
+        # Convert image to BytesIO object
+        image_buffer = BytesIO()
+        image.save(image_buffer, format="PNG")
+        image_buffer.seek(0)
+
+        # Send image to the user who requested
+        await message.reply_photo(image_buffer, caption=f"Here is your Carbon Image {message.from_user.mention} ")
+        await m.delete()
+    else:
+        await message.reply_text("Failed process image ")
+
+@pbot.on_message(filters.command("rayso"), group=646)
+async def rayso_func(_, message):
+    if message.reply_to_message:
+        if message.reply_to_message.text:
+            txt = message.reply_to_message.text
+        else:
+            return await message.reply_text("Please Reply to a message")
     else:
         try:
             txt = message.text.split(None, 1)[1]
         except IndexError:
             return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ.")
     m = await message.reply_text("ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴀʀʙᴏɴ...")
-    response = requests.get(f"https://api.safone.dev/carbon?code=" + txt )
+    response = requests.get(f"https://api.safone.dev/rayso?code=" + txt )
     if response.status_code == 200:  
         data = response.json()
         
@@ -56,10 +93,14 @@ async def carbon_func(_, message):
         await message.reply_text("Failed to decode base64 image data.")
 
 
-__mod_name__ = "Cᴀʀʙᴏɴ"
+
+
+__mod_name__ = "Carbon"
 
 __help__ = """
-ᴍᴀᴋᴇs ᴀ ᴄᴀʀʙᴏɴ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴀɴᴅ sᴇɴᴅ ɪᴛ ᴛᴏ ʏᴏᴜ.
+Makes a Carbon or Rayso of given text and sends it to you 
 
-❍ /carbon *:* ᴍᴀᴋᴇs ᴄᴀʀʙᴏɴ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ.
+ /carbon *:* Makes carbon of replied text .
+ /rayso  *:* makes rayso of the given text .
+
 """
